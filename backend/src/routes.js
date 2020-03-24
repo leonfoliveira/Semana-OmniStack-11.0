d98efbc0b2f70fdfaddbccd25e891d2e301f1e0e
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-24 16:31:17 -0300

    Make migrations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    name: 'Leonardo F Oliveira'
+routes.post('/ngos', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ngos').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
   });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
