9b37c7924f6ba932fcb548bf525b9d6206b31af3
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-24 17:23:46 -0300

    Make session controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,14 @@ const express = require('express');
 const NgoController = require('./controllers/NgoController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.get('/ngos', NgoController.index);
 routes.post('/ngos', NgoController.create);
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/profile', ProfileController.index);
 
