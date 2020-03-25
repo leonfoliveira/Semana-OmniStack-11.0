9ab894f872bf365f05778b92c0f95b53926a1733
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-25 16:32:08 -0300

    Add logon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Logon from './pages/Logon';
 
 function App() {
-  return <Header />;
+  return <Logon />;
 }
 
 export default App;
