b15d8687db4d1c5febf1265eb09c9cdf69f4a334
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-26 16:30:23 -0300

    Add list and detail pages

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
+import Routes from './src/routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return <Routes />;
+}
